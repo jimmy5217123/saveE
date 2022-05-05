0f501864e39658e4b788e5f73c88957c08707a3e
jimmy5217123 <jimmy5217123@gmail.com>
2022-05-05 11:09:35 +0800

    git

---
+++ client/src/layouts/default/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <default-bar />
+
+    <default-drawer />
+
+    <default-view />
+
+    <default-footer />
+
+    <default-settings />
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'DefaultLayout',
+
+    components: {
+      DefaultBar: () => import('./AppBar'),
+      DefaultDrawer: () => import('./Drawer'),
+      DefaultFooter: () => import('./Footer'),
+      DefaultSettings: () => import('./Settings'),
+      DefaultView: () => import('./View'),
+    },
+  }
+</script>
---
+++ client/src/layouts/default/widgets/DrawerHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-list-item class="mb-0 justify-space-between pl-3">
+    <!-- <v-list-item-avatar> -->
+      <v-img
+        max-height="60"
+        max-width="60"
+        style="margin-bottom:7px"
+        :src="require('@/assets/acme-logo.png')"
+      />
+    <!-- </v-list-item-avatar> -->
+
+    <v-list-item-content class="pl-2">
+      <v-list-item-title class="text-h4">
+        <span class="primary--text">雲端儲能監控中心</span>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+  // Utilities
+  import { get } from 'vuex-pathify'
+
+  export default {
+    name: 'DefaultDrawerHeader',
+
+    computed: { version: get('app/version') },
+  }
+</script>
